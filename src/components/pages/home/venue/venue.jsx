5bcedb8bb6162abc6a2e74896cab55361103bb95
Katharina Sick <katharina.sick@dynatrace.com>
2024-07-18 15:04:02 +0200

    chore: Update all dependencies & make hotel cards a bit wider

--- src/components/pages/home/venue/venue.jsx
+++ src/components/pages/home/venue/venue.jsx
@@ -62,11 +62,11 @@ const Venue = () => (
     <div className="flex flex-row flex-wrap justify-center md:flex-col gap-4 mt-12">
       {hotels.map((hotel) => (
         <div
-          className="w-[250px] md:w-full bg-gray-12 rounded-md p-4 flex-col inline-block justify-center">
+          className="w-[400px] md:w-full bg-gray-12 rounded-md p-4 flex-col inline-block justify-center">
           <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">{hotel.discount}</strong>
           <div><a href={hotel.link} target="_blank" className="p-0 m-0 my-2 block text-xl text-center">{hotel.name}</a>
           </div>
-          <p className="text-center" dangerouslySetInnerHTML={{ __html: hotel.description }}></p>
+          <p className="text-center my-2" dangerouslySetInnerHTML={{ __html: hotel.description }} />
         </div>
       ))}
     </div>
