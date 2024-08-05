25148d23fcbfc927e331424aa640ef6f3fa4e1c7
Johannes Grumboeck <johannes.grumboeck@redbull.com>
2024-08-05 14:12:24 +0200

    fix alignment of pill

--- src/components/pages/home/tickets/tickets.jsx
+++ src/components/pages/home/tickets/tickets.jsx
@@ -57,11 +57,13 @@ const Tickets = () => {
       <div className="mt-3 grid grid-cols-2 md:grid-cols-1 gap-2">
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
           <SpeakerProfileImage speakers={workshopSpeakers} speakerId={"bed265e5-76db-4a71-9c0d-c91371e85a87"} className="absolute -left-8 max-h-16 rounded-full cursor-pointer" onClick={() => handleImageClick("bed265e5-76db-4a71-9c0d-c91371e85a87")} />
-          <div className="pl-10 flex flex-col gap-1 items-start text-start">
+          <div className="pl-10 flex flex-col content-between gap-1 h-full">
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 1, 09:00-12:00</strong>
-            <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("687227")}>Build your own Internal Developer Platform with CNOE</h4>
-            <strong>Max Körbächer</strong>
-            <span className="text-balance text-gray-5 text-sm">Technology &amp; Cloud Native Advisor</span>
+            <div className="flex flex-col gap-1 items-start text-start justify-center h-full">
+              <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("687227")}>Build your own Internal Developer Platform with CNOE</h4>
+              <strong>Max Körbächer</strong>
+              <span className="text-balance text-gray-5 text-sm">Technology &amp; Cloud Native Advisor</span>
+            </div>
           </div>
         </div>
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
@@ -69,31 +71,37 @@ const Tickets = () => {
             <SpeakerProfileImage speakers={workshopSpeakers} speakerId={"53714b74-4585-446b-99f7-83406937bac8"} className="max-h-16 rounded-t-full cursor-pointer" onClick={() => handleImageClick("53714b74-4585-446b-99f7-83406937bac8")} />
             <SpeakerProfileImage speakers={workshopSpeakers} speakerId={"cb938518-75cf-4ead-9262-f24323657e4c"} className="max-h-16 rounded-b-full cursor-pointer" onClick={() => handleImageClick("cb938518-75cf-4ead-9262-f24323657e4c")} />
           </div>
-          <div className="pl-10 flex flex-col gap-1 items-start text-start" >
+          <div className="pl-10 flex flex-col content-between gap-1 h-full" >
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 2, 09:00-12:00</strong>
-            <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("701299")}>Detecting unexpected behavior and intrusions with Falco + Atomic Red Team</h4>
-            <strong>Miguel Hernández</strong>
-            <span className="text-balance text-gray-5 text-sm">Staff Threat Researcher Engineer @ Sysdig</span>
-            <strong>Vicente J. Jiménez Miras</strong>
-            <span className="text-balance text-gray-5 text-sm">Developer Advocate &amp; Technical Trainer</span>
+            <div className="flex flex-col gap-1 items-start text-start justify-center h-full">
+              <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("701299")}>Detecting unexpected behavior and intrusions with Falco + Atomic Red Team</h4>
+              <strong>Miguel Hernández</strong>
+              <span className="text-balance text-gray-5 text-sm">Staff Threat Researcher Engineer @ Sysdig</span>
+              <strong>Vicente J. Jiménez Miras</strong>
+              <span className="text-balance text-gray-5 text-sm">Developer Advocate &amp; Technical Trainer</span>
+            </div>
           </div>
         </div>
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
           <SpeakerProfileImage speakers={workshopSpeakers} speakerId={"1829d862-9dc7-49f7-b290-0f69bf61f7d0"} className="absolute -left-8 max-h-16 rounded-full cursor-pointer" onClick={() => handleImageClick("1829d862-9dc7-49f7-b290-0f69bf61f7d0")} />
-          <div className="pl-10 flex flex-col gap-1 items-start text-start">
+          <div className="pl-10 flex flex-col content-between gap-1 h-full">
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 1, 13:00-16:00</strong>
-            <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("712278")}>Deep Dive Into Kubernetes IAM and Network Security: How To Automate at Scale With IBAC and GitOps</h4>
-            <strong>Nic Vermande</strong>
-            <span className="text-balance text-gray-5 text-sm">Head of DevRel @ Otterize</span>
+            <div className="flex flex-col gap-1 items-start text-start justify-center h-full">
+              <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("712278")}>Deep Dive Into Kubernetes IAM and Network Security: How To Automate at Scale With IBAC and GitOps</h4>
+              <strong>Nic Vermande</strong>
+              <span className="text-balance text-gray-5 text-sm">Head of DevRel @ Otterize</span>
+            </div>
           </div>
         </div>
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
           <SpeakerProfileImage speakers={workshopSpeakers} speakerId={"a2665c2b-13c9-4337-9c78-db85bca70e60"} className="absolute -left-8 max-h-16 rounded-full cursor-pointer" onClick={() => handleImageClick("a2665c2b-13c9-4337-9c78-db85bca70e60")} />
-          <div className="pl-10 flex flex-col gap-1 items-start text-start">
+          <div className="pl-10 flex flex-col content-between gap-1 h-full">
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 2, 13:00-16:00</strong>
-            <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("697243")}>Kubernetes Performance Tuning</h4>
-            <strong>Ant(on) Weiss</strong>
-            <span className="text-balance text-gray-5 text-sm">Software Delivery Futurist</span>
+            <div className="flex flex-col gap-1 items-start text-start justify-center h-full">
+              <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick("697243")}>Kubernetes Performance Tuning</h4>
+              <strong>Ant(on) Weiss</strong>
+              <span className="text-balance text-gray-5 text-sm">Software Delivery Futurist</span>
+            </div>
           </div>
         </div>
       </div>
@@ -102,7 +110,7 @@ const Tickets = () => {
       </Button>
       <SpeakerModal isOpen={isSpeakerModalOpen} onClose={closeSpeakerModal} speaker={selectedSpeaker} />
       <SessionModal isOpen={isSessionModalOpen} onClose={closeSessionModal} session={selectedSession} />
-    </section>
+    </section >
   );
 };
 
