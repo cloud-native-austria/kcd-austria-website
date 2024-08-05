bf8277958f21ec95a0023b82925766a04913e8c2
Johannes Grumboeck <johannes.grumboeck@redbull.com>
2024-08-05 11:46:56 +0200

    add modals for speaker and session details

---
+++ src/components/pages/home/speakers/loadWorkshopSessions.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const loadWorkshopSessions = () => {
+    const [workshopSessions, setWorkshopSessions] = useState([]);
+
+    useEffect(() => {
+        const fetchWorkshopSessions = async () => {
+            try {
+                const response = await fetch('https://sessionize.com/api/v2/vdgkc6xy/view/Sessions');
+                const data = await response.json();
+                setWorkshopSessions(data[0].sessions);
+            } catch (error) {
+                console.error('Error fetching sessions:', error);
+            }
+        };
+
+        fetchWorkshopSessions();
+    }, []);
+
+    return { workshopSessions };
+};
+
+export default loadWorkshopSessions;
