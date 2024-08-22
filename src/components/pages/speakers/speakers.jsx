2cc3215f3ef0d95cbc5834415ffc09cc6e0b2d0f
holistic-developer <andreas.taranetz@gmail.com>
2024-08-22 15:28:23 +0200

    Add speaker modal on click

--- src/components/pages/speakers/speakers.jsx
+++ src/components/pages/speakers/speakers.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import loadAllSpeakers from '../../shared/data/loadAllSpeakers';
 import Person from '../../shared/person';
+import SpeakerModal from '../../shared/speaker/speakerModal';
 
 const Speakers = () => {
+  const [selectedSpeaker, setSelectedSpeaker] = useState(null);
+
   const { speakers } = loadAllSpeakers();
+
+  const handleSpeakerClick = (speakerId) => {
+    const speaker = speakers.find(s => s.id === speakerId);
+    setSelectedSpeaker(speaker);
+  };
+
+  const closeSpeakerModal = () => {
+    setSelectedSpeaker(null);
+  };
 
   if (!speakers) {
     return <p>Loading...</p>;
@@ -14,10 +26,13 @@ const Speakers = () => {
     <section className="safe-paddings container-md">
       <h1 id="speakers">Speakers</h1>
       <div className="flex flex-row flex-wrap gap-10 justify-center pt-4">
-        {speakers.map(({ profilePicture, fullName, tagLine }, index) => (
-          <Person name={fullName} tagLine={tagLine} picture={profilePicture} key={index} />
+        {speakers.map(({ profilePicture, fullName, tagLine, id }) => (
+          <a onClick={() => handleSpeakerClick(id)}>
+            <Person name={fullName} tagLine={tagLine} picture={profilePicture} key={id} />
+          </a>
         ))}
       </div>
+      <SpeakerModal isOpen={!!selectedSpeaker} speaker={selectedSpeaker} onClose={closeSpeakerModal} />
     </section>
   );
 };
