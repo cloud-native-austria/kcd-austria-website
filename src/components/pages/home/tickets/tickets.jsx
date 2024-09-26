2b63f95c24731cdb32d2782b7f66f94a0f92dc3d
Johannes Grumboeck <johannes.grumboeck@redbull.com>
2024-09-26 10:26:49 +0200

    chore: change Max to Hossein

--- src/components/pages/home/tickets/tickets.jsx
+++ src/components/pages/home/tickets/tickets.jsx
@@ -59,14 +59,14 @@ const Tickets = () => {
       </p>
       <div className="mt-3 grid grid-cols-2 md:grid-cols-1 gap-2">
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
-          <SpeakerProfileImage speakers={workshopSpeakers} speakerId="bed265e5-76db-4a71-9c0d-c91371e85a87" className="absolute -left-8 max-h-16 rounded-full cursor-pointer"
-                               onClick={() => handleImageClick('bed265e5-76db-4a71-9c0d-c91371e85a87')} />
+          <SpeakerProfileImage speakers={workshopSpeakers} speakerId="214b6e38-faf7-4261-a621-c1063ef39bad" className="absolute -left-8 max-h-16 rounded-full cursor-pointer"
+            onClick={() => handleImageClick('214b6e38-faf7-4261-a621-c1063ef39bad')} />
           <div className="pl-10 flex flex-col content-between gap-1 h-full">
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 1, 09:00-12:00</strong>
             <div className="flex flex-col gap-1 items-start text-start justify-center h-full">
               <h4 className="text-purple font-bold text-balance cursor-pointer" onClick={() => handleSessionClick('687227')}>Build your own Internal Developer Platform with
                 CNOE</h4>
-              <strong>Max Körbächer</strong>
+              <strong>Hossein Salahi</strong>
               <span className="text-balance text-gray-5 text-sm">Technology &amp; Cloud Native Advisor</span>
             </div>
           </div>
@@ -74,9 +74,9 @@ const Tickets = () => {
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
           <div className="absolute -left-8">
             <SpeakerProfileImage speakers={workshopSpeakers} speakerId="53714b74-4585-446b-99f7-83406937bac8" className="max-h-16 rounded-t-full cursor-pointer"
-                                 onClick={() => handleImageClick('53714b74-4585-446b-99f7-83406937bac8')} />
+              onClick={() => handleImageClick('53714b74-4585-446b-99f7-83406937bac8')} />
             <SpeakerProfileImage speakers={workshopSpeakers} speakerId="cb938518-75cf-4ead-9262-f24323657e4c" className="max-h-16 rounded-b-full cursor-pointer"
-                                 onClick={() => handleImageClick('cb938518-75cf-4ead-9262-f24323657e4c')} />
+              onClick={() => handleImageClick('cb938518-75cf-4ead-9262-f24323657e4c')} />
           </div>
           <div className="pl-10 flex flex-col content-between gap-1 h-full">
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 8, 09:00-12:00</strong>
@@ -92,7 +92,7 @@ const Tickets = () => {
         </div>
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
           <SpeakerProfileImage speakers={workshopSpeakers} speakerId="1829d862-9dc7-49f7-b290-0f69bf61f7d0" className="absolute -left-8 max-h-16 rounded-full cursor-pointer"
-                               onClick={() => handleImageClick('1829d862-9dc7-49f7-b290-0f69bf61f7d0')} />
+            onClick={() => handleImageClick('1829d862-9dc7-49f7-b290-0f69bf61f7d0')} />
           <div className="pl-10 flex flex-col content-between gap-1 h-full">
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 1, 13:00-16:00</strong>
             <div className="flex flex-col gap-1 items-start text-start justify-center h-full">
@@ -105,7 +105,7 @@ const Tickets = () => {
         </div>
         <div className="flex items-center m-4 ml-4 p-2 relative bg-gray-12 rounded-md">
           <SpeakerProfileImage speakers={workshopSpeakers} speakerId="a2665c2b-13c9-4337-9c78-db85bca70e60" className="absolute -left-8 max-h-16 rounded-full cursor-pointer"
-                               onClick={() => handleImageClick('a2665c2b-13c9-4337-9c78-db85bca70e60')} />
+            onClick={() => handleImageClick('a2665c2b-13c9-4337-9c78-db85bca70e60')} />
           <div className="pl-10 flex flex-col content-between gap-1 h-full">
             <strong className="text-sm text-start bg-pink text-white py-1 px-2 rounded-full inline-block self-start">Room 8, 13:00-16:00</strong>
             <div className="flex flex-col gap-1 items-start text-start justify-center h-full">
