25abd20ddfd63e4dbe3e5ee27b7ed720e53d08ef
holistic-developer <andreas.taranetz@gmail.com>
2024-10-05 13:32:05 +0200

    Add wifi details

--- src/components/pages/live/live.jsx
+++ src/components/pages/live/live.jsx
@@ -1,55 +1,54 @@
 import React from 'react';
-import Button from '../../shared/button';
+
 import { scheduleUrl } from '../../../constants/sessionize-app';
+import Button from '../../shared/button';
 
 const Live = () => (
-    <>
-      <section className="safe-paddings container-md text-center grid grid-cols-2 sm:grid-cols-1 gap-4">
-        <div className="col-span-2 sm:col-span-1 bg-gray-12 rounded-md p-2 flex-col inline-block justify-center">
-          <h2>Schedule</h2>
-          <p className="text-balance">
-            See the full schedule and mark talks you're interested in so you won't miss a session.
-          </p>
-          <Button to={scheduleUrl} className="my-6 mt-2">Full schedule</Button>
-        </div>
+    <section className="safe-paddings container-md text-center grid grid-cols-2 sm:grid-cols-1 gap-4">
+      <div className="col-span-2 sm:col-span-1 bg-gray-12 rounded-md p-2 flex-col inline-block justify-center">
+        <h2>Schedule</h2>
+        <p className="text-balance">
+          See the full schedule and mark talks you're interested in so you won't miss a session.
+        </p>
+        <Button to={scheduleUrl} className="my-6 mt-2">Full schedule</Button>
+      </div>
 
-        <div className="bg-gray-12 rounded-md p-2 flex-col inline-block justify-center">
-          <h2>Wi-Fi access</h2>
-          <table className="mx-auto border-separate border-spacing-2">
-            <tbody>
-            <tr>
-              <td className="text-right">SSID</td>
-              <td>to be announced</td>
-            </tr>
-            <tr>
-              <td className="text-right">Password</td>
-              <td>to be announced</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
+      <div className="bg-gray-12 rounded-md p-2 flex-col inline-block justify-center">
+        <h2>Wi-Fi access</h2>
+        <table className="mx-auto border-separate border-spacing-2">
+          <tbody>
+          <tr>
+            <td className="text-right">SSID</td>
+            <td>KCDAustria</td>
+          </tr>
+          <tr>
+            <td className="text-right">Password</td>
+            <td>welcome2vienna</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
 
-        <div className="bg-gray-12 rounded-md p-2 flex-col inline-block justify-center">
-          <h2>Emergency numbers</h2>
-          <table className="mx-auto border-separate border-spacing-2">
-            <tbody>
-            <tr>
-              <td className="text-right">Fire service</td>
-              <td><a href="tel:122">122</a></td>
-            </tr>
-            <tr>
-              <td className="text-right">Police</td>
-              <td><a href="tel:133">133</a></td>
-            </tr>
-            <tr>
-              <td className="text-right">Ambulance</td>
-              <td><a href="tel:144">144</a></td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
-    </>
+      <div className="bg-gray-12 rounded-md p-2 flex-col inline-block justify-center">
+        <h2>Emergency numbers</h2>
+        <table className="mx-auto border-separate border-spacing-2">
+          <tbody>
+          <tr>
+            <td className="text-right">Fire service</td>
+            <td><a href="tel:122">122</a></td>
+          </tr>
+          <tr>
+            <td className="text-right">Police</td>
+            <td><a href="tel:133">133</a></td>
+          </tr>
+          <tr>
+            <td className="text-right">Ambulance</td>
+            <td><a href="tel:144">144</a></td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
   )
 ;
 
