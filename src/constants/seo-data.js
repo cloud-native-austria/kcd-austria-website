5c7245413d369efe8652695ab63877dae57b97da
Andreas Taranetz <andreas.taranetz@gmail.com>
2025-02-16 23:12:53 +0100

    Update logo and add new font.
    closes cloud-native-austria/cloud-native-days-austria#7

--- src/constants/seo-data.js
+++ src/constants/seo-data.js
@@ -1,11 +1,11 @@
 export default {
   static: ({ title }) => ({
-    title: `${title} — KCD Austria`,
+    title: `${title} — CND Austria`,
   }),
 
   team: {
-    title: 'Team — KCD Austria',
-    description: 'Experience the power of community at the KCD Austria!',
+    title: 'Team — CND Austria',
+    description: 'Experience the power of community at the CND Austria!',
   },
 
   speakers: {
@@ -14,12 +14,12 @@ export default {
   },
 
   live: {
-    title: 'Live - KCD Austria',
+    title: 'Live - CND Austria',
     description: 'Everything you need to know at the event',
   },
 
   page404: {
-    title: 'Page Not Found — KCD Austria',
+    title: 'Page Not Found — CND Austria',
     description: 'Sorry, we couldn’t find the page you’re looking for',
   }
 };
