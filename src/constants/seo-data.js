f0e1cc1727b754093f3bcc4a4f9bfebece0beeb7
Johannes Grumboeck <johannes.grumboeck@redbull.com>
2025-08-12 16:52:41 +0200

    chore: close volunteer registration by removing the page

--- src/constants/seo-data.js
+++ src/constants/seo-data.js
@@ -18,11 +18,6 @@ export default {
     description: 'Everything you need to know at the event',
   },
 
-  volunteer: {
-    title: 'Volunteer — CND Austria',
-    description: 'Volunteer at the CND Austria and help make the event a success!',
-  },
-
   page404: {
     title: 'Page Not Found — CND Austria',
     description: 'Sorry, we couldn’t find the page you’re looking for',
